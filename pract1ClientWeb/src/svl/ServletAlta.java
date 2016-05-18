6c151efecbdf1fbce948ab9fe616ab7367102de4
Muahahas <eloibuisan@gmail.com>
2016-05-18 18:33:46 +0200

    "Afegits servlets"

--- pract1ClientWeb/src/svl/ServletAlta.java
+++ pract1ClientWeb/src/svl/ServletAlta.java
@@ -1,3 +1,4 @@
+package svl;
 
 
 import java.io.IOException;
@@ -6,6 +7,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * Servlet implementation class ServletAlta
@@ -27,15 +31,42 @@ public class ServletAlta extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		doFer(request, response);
 	}
+
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+		doFer(request, response);
+	}
+	private void doFer(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		String typeForm = request.getParameter("typeForm");
+		System.out.println("HI");
+		if(typeForm=="alta1"){
+			
+			alta1(request,response);
+		}else if(typeForm=="alta2"){
+			throw new NotImplementedException();
+		}
+		
+	}
+	
+	private void alta1(HttpServletRequest request, HttpServletResponse response){
+		HttpSession sessio;
+		
+		String nomLocal = request.getParameter("nomLocal");
+		int typeLocal = Integer.parseInt(request.getParameter("typeLocal"));
+		int typeAddr = Integer.parseInt(request.getParameter("typeAddr"));
+		System.out.println("El nom és: " + nomLocal);
+		System.out.println("L'edat és: " + typeLocal);
+
 	}
 
 }
