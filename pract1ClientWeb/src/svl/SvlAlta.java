0355c12c3ca90bd8fb30aaee9ec463e436412721
Muahahas <eloibuisan@gmail.com>
2016-05-18 18:37:11 +0200

    "Arxius faltants del commit anterior"

---
+++ pract1ClientWeb/src/svl/SvlAlta.java
@@ -0,0 +1,106 @@
+package svl;
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Servlet implementation class SvlAlta
+ */
+@WebServlet({ "/SvlAlta", "/sNewLocal" })
+public class SvlAlta extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SvlAlta() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		doFer(request, response);
+	}
+
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		doFer(request, response);
+	}
+	private void doFer(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+		String typeForm = request.getParameter("typeForm");		
+		if(typeForm.equals("alta1")){			
+			alta1(request,response);
+		}else if(typeForm=="alta2"){
+			throw new NotImplementedException();
+		}
+		
+	}
+	
+	private void alta1(HttpServletRequest request, HttpServletResponse response){
+		HttpSession sessio;
+		
+		String nomLocal = request.getParameter("nomLocal");
+		int typeLocal = Integer.parseInt(request.getParameter("typeLocal"));
+		String nameAddr = request.getParameter("nameAddr");
+		
+		int numAddr =  Integer.parseInt(request.getParameter("num"));
+		String obs = request.getParameter("obs");
+		
+		
+		System.out.println("El nom és: " + nomLocal);
+		System.out.println("El tipus és: " + typeLocal);
+		System.out.println("Nom Addr " + nameAddr);
+		System.out.println("Num Addr: " + numAddr);
+		System.out.println("Les observacions: " + obs);
+	
+		sessio = request.getSession(true);
+		
+		sessio.setAttribute("sessio1.nomLocal", nomLocal);
+		sessio.setAttribute("sessio1.typeLocal", typeLocal);
+		sessio.setAttribute("sessio1.nameAddr", nameAddr);
+		sessio.setAttribute("sessio1.numAddr", numAddr);
+		sessio.setAttribute("sessio1.obs", obs);
+		/*
+		ServletContext context = getServletContext();
+		
+		//DEMANAR AL SERVER LES CARACTERISTIQUES
+		 * 
+		 *
+		 *
+		
+		RequestDispatcher rd = context.getRequestDispatcher("/jFinal");
+
+		try {
+			rd.forward(request, response);
+		} catch (ServletException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+	}
+
+}
--- pract1ClientWeb/src/svl/SvlIndex.java
+++ pract1ClientWeb/src/svl/SvlIndex.java
@@ -1,27 +1,30 @@
 package svl;
 
 
+
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
- * Servlet implementation class ServletAlta
+ * Servlet implementation class SvlIndex
  */
-@WebServlet("/ServletAlta")
-public class ServletAlta extends HttpServlet {
+@WebServlet({ "/SvlIndex", "/sIndex" })
+public class SvlIndex extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServletAlta() {
+    public SvlIndex() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,10 +35,8 @@ public class ServletAlta extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		doFer(request, response);
+		doFer(request,response);
 	}
-
-	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -43,30 +44,28 @@ public class ServletAlta extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//doGet(request, response);
-		doFer(request, response);
-	}
-	private void doFer(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		String typeForm = request.getParameter("typeForm");
-		System.out.println("HI");
-		if(typeForm=="alta1"){
-			
-			alta1(request,response);
-		}else if(typeForm=="alta2"){
-			throw new NotImplementedException();
-		}
-		
+		doFer(request,response);
 	}
 	
-	private void alta1(HttpServletRequest request, HttpServletResponse response){
-		HttpSession sessio;
+	private void doFer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/**System.out.println("Hola");
+		String typeForm = request.getParameter("type");	
+		System.out.println(typeForm);
+		if(typeForm.equals("alta")){			
+			System.out.println("alta");
+		}else if(typeForm=="cerca"){
+			System.out.println("cerca");
+		}*/
+		ServletContext context = getServletContext();
+		RequestDispatcher rd = context.getRequestDispatcher("/jFinal");
+
+		try {
+			rd.forward(request, response);
+		} catch (ServletException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		String nomLocal = request.getParameter("nomLocal");
-		int typeLocal = Integer.parseInt(request.getParameter("typeLocal"));
-		int typeAddr = Integer.parseInt(request.getParameter("typeAddr"));
-		System.out.println("El nom és: " + nomLocal);
-		System.out.println("L'edat és: " + typeLocal);
-
+	
 	}
-
 }
